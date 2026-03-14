4bbb79bd082cecab01985f30abea5aa93e980566
Andrei <andrei.lerke@gmail.com>
2026-03-14 21:55:58 +0100

    led pins updated

--- src/config.h
+++ src/config.h
@@ -2,7 +2,10 @@
 
 /** Pins */
 #define RED_LED_PIN 0
-#define WHITE_LED_PIN 1
+#define WHITE_LED_1_PIN 5
+#define WHITE_LED_2_PIN 6
+#define WHITE_LED_3_PIN 7
+#define SOIL_MOISTURE_PIN 1
 
 /** Sensors */
 bool hasDHT22Sensor = true;
