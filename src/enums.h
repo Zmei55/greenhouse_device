6aadf560c459ab8b53a80863635a5758c968c99e
Andrei <andrei.lerke@gmail.com>
2026-03-15 15:52:49 +0100

    time enum updated

--- src/enums.h
+++ src/enums.h
@@ -4,5 +4,6 @@ enum TimeApp: unsigned long {
     ONE_SECOND = 1000,
     ONE_MINUTE = ONE_SECOND * 60,
     FIFTEEN_MINUTE = ONE_MINUTE * 15,
+    HALF_HOUR = ONE_MINUTE * 30,
     ONE_HOUR = ONE_MINUTE * 60,
 };
