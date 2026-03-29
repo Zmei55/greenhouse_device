212ffb4012c8d99d4fcad57f29651f0d600a2b05
Andrei <andrei.lerke@gmail.com>
2026-03-29 13:49:36 +0200

    обновлена работа датчика влажности и добавлены комментарии

--- src/config.cpp
+++ src/config.cpp
@@ -4,7 +4,8 @@
 
 /** Настройка пинов для ESP32_C3 */
 #if defined(ESP32)
-// uint8_t RED_LED_PIN = 0;
+// uint8_t MOTOR_IN_1_PIN = 0;
+// uint8_t MOTOR_IN_2_PIN = 1;
 uint8_t SOIL_MOISTURE_PIN = 3;
 uint8_t LED_STRIP_ONE_PIN = 5;
 uint8_t LED_STRIP_TWO_PIN = 6;
@@ -31,11 +32,11 @@ uint16_t soilMoistureDryValue = 3750;
 uint16_t soilMoistureWetValue = 3600;
 
 /** Time config */
-const struct StartTime {
+struct StartTime {
     uint8_t hour = 9; // Час включения аппарата (09)
     uint8_t minute = 0; // Минуты включения аппарата (00)
 } start;
-const struct EndTime {
+struct EndTime {
     uint8_t hour = 20; // Час выключения аппарата (20)
     uint8_t minute = 0; // Минуты включения аппарата (00)
 } end;
