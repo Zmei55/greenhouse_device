6c66d63e216be53f52d83351fcfea292287228a8
Andrei <andrei.lerke@gmail.com>
2026-04-07 20:11:39 +0200

    api рабочее время get/post добавлены

--- src/api.h
+++ src/api.h
@@ -42,12 +42,12 @@ void apiHandler(){
         JsonObject workingHours = data["workingHours"].to<JsonObject>();
 
         JsonObject startTime = workingHours["start"].to<JsonObject>();
-        startTime["hour"] = start.getHour();
-        startTime["minute"] = start.getMinute();
+        startTime["hour"] = start->getHour();
+        startTime["minute"] = start->getMinute();
 
         JsonObject endTime = workingHours["end"].to<JsonObject>();        
-        endTime["hour"] = end.getHour();
-        endTime["minute"] = end.getMinute();
+        endTime["hour"] = end->getHour();
+        endTime["minute"] = end->getMinute();
 
         request->send(200, "application/json", data.as<String>());
     });
@@ -65,6 +65,51 @@ void apiHandler(){
         JsonDocument data;
 
         data["deviceDateTime"] = "Not implemented yet";
+
+        request->send(200, "application/json", data.as<String>());
+    });
+
+    server.on("/working-time", HTTP_GET, [](AsyncWebServerRequest *request){
+        JsonDocument data;
+        data["workingHours"] = nullptr;
+
+        /** Новое рабочее время аппарата */
+        JsonObject newWorkingHours = data["workingHours"].to<JsonObject>();
+
+        JsonObject startTime = newWorkingHours["start"].to<JsonObject>();
+        startTime["hour"] = start->getHour();
+        startTime["minute"] = start->getMinute();
+
+        JsonObject endTime = newWorkingHours["end"].to<JsonObject>();        
+        endTime["hour"] = end->getHour();
+        endTime["minute"] = end->getMinute();
+
+        request->send(200, "application/json", data.as<String>());
+    });
+
+    server.on("/working-time", HTTP_POST, [](AsyncWebServerRequest *request, JsonVariant &json){
+        JsonDocument data;
+        data["workingHours"] = nullptr;
+        
+        JsonObject body = json.as<JsonObject>();
+        uint8_t start_hour = body["start"]["hour"];
+        uint8_t start_minute = body["start"]["minute"];
+        uint8_t end_hour = body["end"]["hour"];
+        uint8_t end_minute = body["end"]["minute"];
+
+        start->update(start_hour, start_minute);
+        end->update(end_hour, end_minute);
+
+        /** Новое рабочее время аппарата */
+        JsonObject newWorkingHours = data["workingHours"].to<JsonObject>();
+
+        JsonObject startTime = newWorkingHours["start"].to<JsonObject>();
+        startTime["hour"] = start->getHour();
+        startTime["minute"] = start->getMinute();
+
+        JsonObject endTime = newWorkingHours["end"].to<JsonObject>();        
+        endTime["hour"] = end->getHour();
+        endTime["minute"] = end->getMinute();
 
         request->send(200, "application/json", data.as<String>());
     });
