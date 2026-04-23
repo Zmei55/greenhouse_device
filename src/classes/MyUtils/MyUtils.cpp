2e06e11ed829bf8b6940ba20adcc718c15a38733
Andrei <andrei.lerke@gmail.com>
2026-04-23 18:52:57 +0200

    указатели заменены на ссылки, потому что это правильнее

--- src/classes/MyUtils/MyUtils.cpp
+++ src/classes/MyUtils/MyUtils.cpp
@@ -6,34 +6,34 @@
 
 MyUtils::MyUtils() {}
 
-uint32_t MyUtils::getNowTimeToInt(const DateTime& time) {
+uint32_t MyUtils::getNowTimeToInt(const DateTime &time) {
     return time.hour() * 3600 + time.minute() * 60;
 }
 
-void MyUtils::enablingLighting(bool *flag) {
+void MyUtils::enablingLighting(bool &flag) {
     digitalWrite(LED_STRIP_ONE_PIN, HIGH);
     digitalWrite(LED_STRIP_TWO_PIN, HIGH);
     digitalWrite(LED_STRIP_THREE_PIN, HIGH);
-    *flag = true;
+    flag = true;
 }
 
-void MyUtils::disablingLighting(bool *flag) {
+void MyUtils::disablingLighting(bool &flag) {
     digitalWrite(LED_STRIP_ONE_PIN, LOW);
     digitalWrite(LED_STRIP_TWO_PIN, LOW);
     digitalWrite(LED_STRIP_THREE_PIN, LOW);
-    *flag = false;
+    flag = false;
 }
 
-void MyUtils::enablingWatering(bool *flag) {
+void MyUtils::enablingWatering(bool &flag) {
     // digitalWrite(WATER_PIN, HIGH);
-    *flag = true;
+    flag = true;
 }
 
-void MyUtils::disablingWatering(bool *flag) {
+void MyUtils::disablingWatering(bool &flag) {
     // digitalWrite(WATER_PIN, LOW);
-    *flag = false;
+    flag = false;
 }
 
-void MyUtils::openingWindow(bool *flag) {};
+void MyUtils::openingWindow(bool &flag) {};
 
-void MyUtils::closingWindow(bool *flag) {};
+void MyUtils::closingWindow(bool &flag) {};
--- src/classes/MyUtils/MyUtils.h
+++ src/classes/MyUtils/MyUtils.h
@@ -12,35 +12,35 @@ public:
     /** Переводит время из DateTime в целое число
      * @param time экземпляр класса DateTime
      */
-    uint32_t getNowTimeToInt(const DateTime& time);
+    uint32_t getNowTimeToInt(const DateTime &time);
 
     /** Включение освещения
      * @param flag включено ли освещение (bool)
      */
-    void enablingLighting(bool *flag);
+    void enablingLighting(bool &flag);
 
     /** Выключение освещения
      * @param flag включено ли освещение (bool)
      */
-    void disablingLighting(bool *flag);
+    void disablingLighting(bool &flag);
 
     /** Включение полива
      * @param flag включен ли полив (bool)
      */
-    void enablingWatering(bool *flag);
+    void enablingWatering(bool &flag);
 
     /** Выключение полива
      * @param flag включен ли полив (bool)
      */
-    void disablingWatering(bool *flag);
+    void disablingWatering(bool &flag);
 
     /** Открывание окна
      * @param flag включен ли мотор (bool)
      */
-    void openingWindow(bool *flag);
+    void openingWindow(bool &flag);
 
     /** Закрывание окна
      * @param flag включен ли мотор (bool)
      */
-    void closingWindow(bool *flag);
+    void closingWindow(bool &flag);
 };
